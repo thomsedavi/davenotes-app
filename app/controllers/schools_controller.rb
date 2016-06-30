87837553fb81d52513483e539d8e85481519d4ba
thomsedavi <thomsedavi@yahoo.co.nz>
2016-07-01 00:29:04 +1200

    created a School object

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,25 @@
+class SchoolsController < ApplicationController
+  def index
+    @schools = School.all
+  end
+
+  def show
+    @school = School.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @school = School.new(school_params)
+
+    @school.save
+    redirect_to @school
+  end
+
+  private
+  def school_params
+    params.require(:school).permit(:name, :country, :website)
+  end
+end
