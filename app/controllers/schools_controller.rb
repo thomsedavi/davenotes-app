7a1b769897cae4382510960a16484dfbfae0972f
thomsedavi <thomsedavi@yahoo.co.nz>
2016-07-05 00:04:12 +1200

    pagination for countries

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,6 +1,6 @@
 class SchoolsController < ApplicationController
   def index
-    @schools = School.all
+    @schools = School.where(:active => true).paginate(:page => params[:page], :per_page => 5).order('name ASC, country ASC')
   end
 
   def show
