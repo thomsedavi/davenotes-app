2a4f0388e54a156c5001a5ffa359ce25951b4ca6
thomsedavi <thomsedavi@yahoo.co.nz>
2016-07-06 03:30:39 +1200

    created some static pages, also started adding tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+
   get 'schools/:id/courses', to: 'courses#index'
   get 'schools/:id/courses/new', to: 'courses#new'
 
