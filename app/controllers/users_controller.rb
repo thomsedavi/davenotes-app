ab99778a1fab658066ba8d0dfc3df6ff28bd06c6
thomsedavi <thomsedavi@yahoo.co.nz>
2016-07-13 20:57:37 +1200

    only admin can see list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :correct_user, only: [:edit, :update, :show]
+  before_action :admin_user, only: [:destroy, :index]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
+    redirect_to(root_url) unless current_user?(@user) or current_user.admin?
   end
 
   # Confirms an admin user.
