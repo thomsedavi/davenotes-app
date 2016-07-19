bf4df49ae45de5899d2510a55bf68752589b6b43
thomsedavi <thomsedavi@yahoo.co.nz>
2016-07-19 23:21:33 +1200

    course admin can confirm or deny requests to edit

--- app/controllers/course_users_controller.rb
+++ app/controllers/course_users_controller.rb
@@ -1,8 +1,22 @@
 class CourseUsersController < ApplicationController
+  before_action :admin_user, only: [:edit]
+
   def new
     @course = Course.find(params[:course])
     @course_user = CourseUser.new(course: @course, user: current_user)
     @course_user.save
+    redirect_to @course
+  end
+
+  def edit
+    @course_user = CourseUser.find(params[:id])
+    @course = @course_user.course
+    if params[:method] == 'delete'
+      @course_user.destroy
+    elsif params[:method] == 'confirm'
+      @course_user.active = true
+      @course_user.save
+    end
     redirect_to @course
   end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,6 +7,7 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
     @school = School.find(@course.school_id)
+    @course_users = CourseUser.where(course: @course, active: false)
   end
 
   def new
